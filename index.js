b1c97adbb207c1612a4d077694865deaea52aad8
Lance Kelley <lancekkcodes@gmail.com>
2021-10-24 15:43:12 -0500

    initial routes

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json("hello world");
+const options = { root: path.join(__dirname) };
+
+app.get('/notes', (req, res) => {
+  res.sendFile('/public/notes.html', options);
 });
 
+app.get('*', (req, res) => {
+  res.sendFile('/public/index.html', options);
+});
+
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json').then(f => f.json()).then(json => {
+    res.json(json);
+  });
+});
+
+app.post('/api/notes', (req, res) => {
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port);
