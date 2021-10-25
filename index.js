220b3948dd0b15de82857550b78dc60fa3c748b0
Lance Kelley <lancekkcodes@gmail.com>
2021-10-24 20:05:52 -0500

    uuid, middleware, send db.json

--- index.js
+++ index.js
@@ -1,39 +1,45 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const { nextTick } = require('process');
+const {v4: uuidv4} = require('uuid');
 const app = express();
+
+app.use(express.json());
+app.use('/', express.static(__dirname + '/public'));
 
 const options = { root: path.join(__dirname) };
 
 const logger = (method, route, filename) => {
-  (err) => {
+  return (err) => {
     if (err) {
       next(err);
     } else {
       console.log(`replied to ${method} on ${route} with ${filename}`);
     }
-  }
+  };
 }
 
 app.get('/notes', (req, res) => {
   res.sendFile('/public/notes.html', options, logger('get', '/notes', '/public/notes.html'));
 });
 
-app.get('*', (req, res) => {
-  res.sendFile('/public/index.html', options, logger('get', req.path, '/public/index.html'));
-});
-
 app.get('/api/notes', (req, res) => {
-  fs.readFile('./db/db.json').then(f => f.json()).then(json => {
-    res.json(json);
-  }).catch(logger('get', req.path, '/db/db.json'));
+  res.sendFile('/db/db.json', options, logger('get', req.path, '/db/db.json'));
 });
 
 app.post('/api/notes', (req, res) => {
+  console.log(req.body);
+  let id = new uuidv4();
+  console.log(`Created uuidv4: ${id}`);
 });
 
 app.delete('/api/notes/:id', (req, res) => {
+});
+
+app.get('')
+
+app.get('*', (req, res) => {
+  res.sendFile('/public/index.html', options, logger('get', req.path, '/public/index.html'));
 });
 
 const port = process.env.PORT || 5000;
